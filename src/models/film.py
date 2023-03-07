d96df6aa6f2eeb2a36c81ae46bcf63c80829db35
dimitrieidifesk <123076304+dimitrieidifesk@users.noreply.github.com>
2023-03-07 21:48:01 +0300

    fix: removed mixin class

--- src/models/film.py
+++ src/models/film.py
@@ -1,39 +1,61 @@
+# Ранее вы уже создавали модель Film в api/v1/films.py,
+# но она используется исключительно для представления данных по HTTP.
+# Внутренние модели, одну из которых вы создаёте сейчас,
+# используется только в рамках бизнес-логики.
+
 import orjson
+
+# Используем pydantic для упрощения работы при перегонке данных из json в объекты
 from pydantic import BaseModel
 
 
 def orjson_dumps(v, *, default):
+    # orjson.dumps возвращает bytes, а pydantic требует unicode, поэтому декодируем
     return orjson.dumps(v, default=default).decode()
 
 
-class ConfigMixin(BaseModel):
+class Film(BaseModel):
+    id: str
+    title: str
+    description: str
+
     class Config:
         json_loads = orjson.loads
         json_dumps = orjson_dumps
 
 
-class Film(BaseModel, ConfigMixin):
-    id: str
-    title: str
-    description: str
-
-
-class Genres(BaseModel, ConfigMixin):
+class Genres(BaseModel):
     id: str
     name: str
     description: str
 
+    class Config:
+        json_loads = orjson.loads
+        json_dumps = orjson_dumps
 
-class Actors(BaseModel, ConfigMixin):
+
+class Actors(BaseModel):
     id: str
     full_name: str
 
+    class Config:
+        json_loads = orjson.loads
+        json_dumps = orjson_dumps
 
-class Writers(BaseModel, ConfigMixin):
+
+class Writers(BaseModel):
     id: str
     full_name: str
 
+    class Config:
+        json_loads = orjson.loads
+        json_dumps = orjson_dumps
 
-class Directors(BaseModel, ConfigMixin):
+
+class Directors(BaseModel):
     id: str
     full_name: str
+
+    class Config:
+        json_loads = orjson.loads
+        json_dumps = orjson_dumps
