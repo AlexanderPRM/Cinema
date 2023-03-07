c8bf4a29aaab2559332ea5d307de7fac1bc04780
dimitrieidifesk <123076304+dimitrieidifesk@users.noreply.github.com>
2023-03-07 21:49:52 +0300

    feat: Removed comments

--- src/models/film.py
+++ src/models/film.py
@@ -1,16 +1,9 @@
-# Ранее вы уже создавали модель Film в api/v1/films.py,
-# но она используется исключительно для представления данных по HTTP.
-# Внутренние модели, одну из которых вы создаёте сейчас,
-# используется только в рамках бизнес-логики.
-
 import orjson
 
-# Используем pydantic для упрощения работы при перегонке данных из json в объекты
 from pydantic import BaseModel
 
 
 def orjson_dumps(v, *, default):
-    # orjson.dumps возвращает bytes, а pydantic требует unicode, поэтому декодируем
     return orjson.dumps(v, default=default).decode()
 
 
