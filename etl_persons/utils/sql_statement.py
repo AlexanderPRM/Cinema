112b2a06ed05b46847400fd1ab91db72c3bd3e97
Alex Shapovalov <kerrolean@yandex.ru>
2023-03-10 19:38:03 +0300

    changed from movies title to movies UUID

--- etl_persons/utils/sql_statement.py
+++ etl_persons/utils/sql_statement.py
@@ -3,13 +3,13 @@ STMT = """
         p.id,
         p.full_name,
         string_agg(
-            DISTINCT CASE WHEN pfw.role = 'director' THEN fw.title ELSE '' END, ','
+            DISTINCT CASE WHEN pfw.role = 'director' THEN fw.id::varchar ELSE '' END, ','
         ) AS director,
         string_agg(
-            DISTINCT CASE WHEN pfw.role = 'actor' THEN fw.title ELSE '' END, ','
+            DISTINCT CASE WHEN pfw.role = 'actor' THEN fw.id::varchar ELSE '' END, ','
         ) AS actors,
         string_agg(
-            DISTINCT CASE WHEN pfw.role = 'writer' THEN fw.title ELSE '' END, ','
+            DISTINCT CASE WHEN pfw.role = 'writer' THEN fw.id::varchar ELSE '' END, ','
         ) AS writers,
         MAX(p.updated_at) AS last_modified
     FROM
