66b8f03651cc1b6dbdc4b8bced9cf08c35ce178c
dimitrieidifesk <123076304+dimitrieidifesk@users.noreply.github.com>
2023-03-13 21:52:10 +0300

    add(film): caching film_details in redis

--- src/api/v1/films.py
+++ src/api/v1/films.py
@@ -1,4 +1,6 @@
 from http import HTTPStatus
+from typing import Dict, List, Optional
+from uuid import UUID
 
 from fastapi import APIRouter, Depends, HTTPException
 from pydantic import BaseModel
@@ -11,29 +13,66 @@ router = APIRouter()
 class Film(BaseModel):
     id: str
     title: str
+    imdb_rating: float
+
+
+class FilmDetail(BaseModel):
+    id: str
+    title: str
+    imdb_rating: float
+    desription: Optional[str]
+    genre: List[Optional[str]]
+    actors: List[Optional[dict]]
+    writers: List[Optional[dict]]
+    director: List[Optional[str]]
 
 
 # Внедряем FilmService с помощью Depends(get_film_service)
 @router.get(
     "/{film_id}",
-    response_model=Film,
-    summary="Список фильмов",
-    description=(
-        "Список фильмов с пагинацией, фильтрацией по жанрам" "и сортировкой по названию и рейтингу"
-    ),
+    response_model=FilmDetail,
+    response_description="Пример вернушегося фильма",
+    summary="Фильм",
+    description=("Получение одного фильма по ID"),
 )
-async def film_details(film_id: str, film_service: FilmService = Depends(get_film_service)) -> Film:
-    film = await film_service.get_by_id(film_id)
+async def film_details(
+    film_id: str, film_service: FilmService = Depends(get_film_service)
+) -> FilmDetail:
+    film = await film_service._film_from_cache(film_id)
+    if film is None:
+        film = await film_service.get_by_id(film_id)
+        film_service._put_film_to_cache(film)
     if not film:
-        # Если фильм не найден, отдаём 404 статус
-        # Желательно пользоваться уже определёнными HTTP-статусами, которые содержат enum
-        # Такой код будет более поддерживаемым
-        raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail="film not found")
+        raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail="Film Not Found")
+    return FilmDetail(
+        id=film.id,
+        title=film.title,
+        imdb_rating=film.imdb_rating,
+        desription=film.description,
+        genre=film.genre,
+        actors=film.actors,
+        writers=film.writers,
+        director=film.director,
+    )
 
-    # Перекладываем данные из models.Film в Film
-    # Обратите внимание, что у модели бизнес-логики есть поле description
-    # Которое отсутствует в модели ответа API.
-    # Если бы использовалась общая модель для бизнес-логики и формирования ответов API
-    # вы бы предоставляли клиентам данные, которые им не нужны
-    # и, возможно, данные, которые опасно возвращать
-    return Film(id=film.id, title=film.title)
+
+@router.get(
+    "",
+    response_model=List[Optional[Dict[str, Film]]],
+    response_description="Пример вернувшегося списка фильмов.",
+    response_model_exclude=["description", "genre", "actors", "writers", "director"],
+    summary="Список фильмов",
+    description="Список фильмов с пагинацией, "
+    "фильтрацией по жанрам и сортировкой по названию или рейтингу.",
+)
+async def films(
+    film_service: FilmService = Depends(get_film_service),
+    sort: str = "-imdb_rating",
+    genre: Optional[UUID] = None,
+    page_number: Optional[int] = 1,
+    page_size: Optional[int] = 10,
+) -> Optional[List[Dict[str, Film]]]:
+    films = await film_service.get_films(sort, genre, page_number, page_size)
+    if not films:
+        raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail="Films Not Found")
+    return films
