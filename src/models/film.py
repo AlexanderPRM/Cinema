1460d22ebc8a73496f6db2ede23a199e32264e15
dimitrieidifesk <123076304+dimitrieidifesk@users.noreply.github.com>
2023-03-13 21:54:11 +0300

    fix (Film(BaseModel)): fix genre's field type
    List[dict] to List[str]

--- src/models/film.py
+++ src/models/film.py
@@ -1,20 +1,64 @@
-import orjson
+from typing import List
 
-# Используем pydantic для упрощения работы при перегонке данных из json в объекты
+import orjson
 from pydantic import BaseModel
 
 
 def orjson_dumps(v, *, default):
-    # orjson.dumps возвращает bytes, а pydantic требует unicode, поэтому декодируем
     return orjson.dumps(v, default=default).decode()
 
 
 class Film(BaseModel):
     id: str
     title: str
+    imdb_rating: float
     description: str
+    genre: list[str]
+    actors: list[dict]
+    writers: list[dict]
+    director: list
 
     class Config:
         # Заменяем стандартную работу с json на более быструю
+        json_loads = orjson.loads
+        json_dumps = orjson_dumps
+
+
+class Genres(BaseModel):
+    id: str
+    name: str
+    desription: str
+
+    class Config:
+        json_loads = orjson.loads
+        json_dumps = orjson_dumps
+
+
+class Actor(BaseModel):
+    id: str
+    full_name: str
+    films: List[str]
+
+    class Config:
+        json_loads = orjson.loads
+        json_dumps = orjson_dumps
+
+
+class Director(BaseModel):
+    id: str
+    full_name: str
+    films: List[str]
+
+    class Config:
+        json_loads = orjson.loads
+        json_dumps = orjson_dumps
+
+
+class Writer(BaseModel):
+    id: str
+    full_name: str
+    films: List[str]
+
+    class Config:
         json_loads = orjson.loads
         json_dumps = orjson_dumps
