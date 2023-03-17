c9d90afcf463de606d73144f7222e2db55f0c6ce
dimitrieidifesk <123076304+dimitrieidifesk@users.noreply.github.com>
2023-03-17 19:16:43 +0300

    fix (redis_db): review fixing #7

--- src/db/redis_db.py
+++ src/db/redis_db.py
@@ -1,11 +1,8 @@
-import os
+from typing import Optional
+from redis.asyncio import Redis
 
-from dotenv import load_dotenv
-from redis import Redis, StrictRedis
-
-load_dotenv()
-
+redis: Optional[Redis] = None
 
 async def get_redis() -> Redis:
-    con = StrictRedis(host=os.environ.get("REDIS_HOST"), port=os.environ.get("REDIS_PORT"), db=0)
-    return con
+    return redis 
+
