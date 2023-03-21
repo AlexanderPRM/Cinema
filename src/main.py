c2ba7663320dc6e8e28443b14306cea516e5eca9
dimitrieidifesk <123076304+dimitrieidifesk@users.noreply.github.com>
2023-03-21 12:37:45 +0300

    fix (main): fix start errors

--- src/main.py
+++ src/main.py
@@ -1,16 +1,20 @@
+import logging
+
+import uvicorn
 from elasticsearch import AsyncElasticsearch
 from fastapi import FastAPI
 from fastapi.responses import ORJSONResponse
 from redis.asyncio import Redis
 
 from api.v1 import films, genres, persons
-from core import config
+from core.config import config
+from core.logger import LOGGING
 from db import elastic, redis_db
 
 app = FastAPI(
-    title=config.config.PROJECT_NAME,
+    title=config.PROJECT_NAME,
     description="Информация о фильмах, жанрах и людях, участвовавших в создании произведения",
-    version=config.config.PROJECT_VERSION,
+    version=config.PROJECT_VERSION,
     docs_url="/api/openapi",
     openapi_url="/api/openapi.json",
     default_response_class=ORJSONResponse,
@@ -22,10 +26,8 @@ async def startup():
     # Подключаемся к базам при старте сервера
     # Подключиться можем при работающем event-loop
     # Поэтому логика подключения происходит в асинхронной функции
-    redis_db.redis = Redis(host=config.config.REDIS_HOST, port=config.config.REDIS_PORT)
-    elastic.es = AsyncElasticsearch(
-        hosts=[f"{config.config.ELASTIC_HOST}:{config.config.ELASTIC_PORT}"]
-    )
+    redis_db.redis = Redis(host=config.REDIS_HOST, port=config.REDIS_PORT)
+    elastic.es = AsyncElasticsearch(hosts=[f"{config.ELASTIC_HOST}:{config.ELASTIC_PORT}"])
 
 
 @app.on_event("shutdown")
@@ -41,17 +43,11 @@ app.include_router(films.router, prefix="/api/v1/films", tags=["Фильмы"])
 app.include_router(genres.router, prefix="/api/v1/genres", tags=["Жанры"])
 app.include_router(persons.router, prefix="/api/v1/persons", tags=["Персонажи"])
 
-
-# Для запуска через uvicorn
-
-# import logging
-# from core.logger import LOGGING
-# import uvicorn
-# if __name__ == "__main__":
-#     uvicorn.run(
-#         "main:app",
-#         host="0.0.0.0",
-#         port=8000,
-#         log_config=LOGGING,
-#         log_level=logging.DEBUG,
-#     )
+if __name__ == "__main__":
+    uvicorn.run(
+        "main:app",
+        host="0.0.0.0",
+        port=8000,
+        log_config=LOGGING,
+        log_level=logging.DEBUG,
+    )
