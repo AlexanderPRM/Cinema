1e434cd46ee2e797725f2de594edfa30df026566
AlexanderPRM <nathan920@yandex.ru>
2023-03-24 21:25:10 +0300

    add(tests):add tests infrastructure

---
+++ tests/functional/conftest.py
@@ -0,0 +1,66 @@
+import asyncio
+import json
+from typing import List
+
+import aiohttp
+import pytest
+import pytest_asyncio
+from elasticsearch import AsyncElasticsearch
+
+from . import settings
+from .settings import films_settings
+
+
+def get_es_bulk_query(es_data, es_index, es_id_field):
+    bulk_query = []
+    for row in es_data:
+        bulk_query.extend(
+            [json.dumps({"index": {"_index": es_index, "_id": row[es_id_field]}}), json.dumps(row)]
+        )
+    return bulk_query
+
+
+@pytest.fixture(scope="session")
+def event_loop():
+    """Overrides pytest default function scoped event loop"""
+    policy = asyncio.get_event_loop_policy()
+    loop = policy.new_event_loop()
+    yield loop
+    loop.close()
+
+
+@pytest_asyncio.fixture(scope="session", autouse=True)
+async def es_client():
+    client = AsyncElasticsearch(hosts=films_settings.es_adress, validate_cert=False, use_ssl=False)
+    yield client
+    await client.close()
+
+
+@pytest_asyncio.fixture(scope="session", autouse=True)
+async def aiohttp_client():
+    client = aiohttp.ClientSession()
+    yield client
+    await client.close()
+
+
+@pytest.fixture
+def es_write_films_data(es_client):
+    async def inner(data: List[dict]):
+        bulk_query = get_es_bulk_query(data, films_settings.es_index, films_settings.es_id_field)
+        str_query = "\n".join(bulk_query) + "\n"
+        response = await es_client.bulk(str_query, refresh=True)
+        if response["errors"]:
+            raise Exception("Ошибка записи данных в Elasticsearch")
+
+    return inner
+
+
+@pytest.fixture
+def make_get_request(aiohttp_client):
+    async def inner(url, query_data):
+        url = settings.films_settings.service_url + url
+        query_data = query_data
+        response = await aiohttp_client.get(url, params=query_data)
+        return response
+
+    return inner
---
+++ tests/functional/src/test_search.py
@@ -0,0 +1,59 @@
+import datetime
+import uuid
+
+import pytest
+
+#  Название теста должно начинаться со слова `test_`
+#  Любой тест с асинхронными вызовами нужно оборачивать декоратором `pytest.mark.asyncio`,
+#  который следит за запуском и работой цикла событий.
+
+
+@pytest.mark.parametrize(
+    "query_data, expected_answer",
+    [
+        ({"query": "The Star", "page_size": 50}, {"status": 200, "length": 50, "exists": True}),
+        (
+            {"query": "Mashed potato", "page_size": 50},
+            {"status": 404, "length": 0, "exists": False},
+        ),
+    ],
+)
+@pytest.mark.asyncio
+async def test_search(make_get_request, es_write_films_data, query_data, expected_answer):
+    # 1. Генерируем данные для ES
+
+    es_data = [
+        {
+            "id": str(uuid.uuid4()),
+            "imdb_rating": 8.5,
+            "genre": ["Action", "Sci-Fi"],
+            "title": "The Star",
+            "description": "New World",
+            "director": ["Stan"],
+            "actors_names": ["Ann", "Bob"],
+            "writers_names": ["Ben", "Howard"],
+            "actors": [{"id": "111", "name": "Ann"}, {"id": "222", "name": "Bob"}],
+            "writers": [{"id": "333", "name": "Ben"}, {"id": "444", "name": "Howard"}],
+            "created_at": datetime.datetime.now().isoformat(),
+            "updated_at": datetime.datetime.now().isoformat(),
+            "film_work_type": "movie",
+        }
+        for _ in range(60)
+    ]
+
+    await es_write_films_data(es_data)
+
+    # 3. Запрашиваем данные из ES по API
+
+    response = await make_get_request("/api/v1/films/search", query_data)
+    body = await response.json()
+    status = response.status
+
+    # 4. Проверяем ответ
+    print(body)
+    assert status == expected_answer["status"]
+    if expected_answer["exists"]:
+        assert len(body) == expected_answer["length"]
+    else:
+        body.pop("detail")
+        assert len(body) == expected_answer["length"]
