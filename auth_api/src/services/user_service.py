2594af73cf7e10781807716a301387d1a50518eb
dimitrieidifesk <123076304+dimitrieidifesk@users.noreply.github.com>
2023-04-13 20:00:51 +0300

    add(profile): profile tools

--- auth_api/src/services/user_service.py
+++ auth_api/src/services/user_service.py
@@ -21,11 +21,11 @@ class UserService:  # Унаследовать надо будет
         pass
 
     def signup(self, email, password, name):
+        email = self.normalize_email(email)
         try:
             validate_email(email)
         except EmailError:
             return abort(Response(json.dumps({"message": "Email is not correct."}), 422))
-        email = self.normalize_email(email)
         if db.session.query(User).filter_by(email=email).first():
             return abort(Response(json.dumps({"message": f"User {email} already exists."}), 422))
 
@@ -46,8 +46,33 @@ class UserService:  # Унаследовать надо будет
             db.session.commit()
         return email, password, role, user
 
-    def change_password():
-        pass
+    def refresh(self, email):
+        user = User.query.filter_by(email=email).first()
+        return user.id
 
-    def change_email():
-        pass
+    def get_profile_info(self, email):
+        user = User.query.filter_by(email=email).first()
+        return user
+
+    def change_name(self, email, new_name):
+        db.session.query(User).filter(User.email.ilike(email)).update(
+            {"name": new_name}, synchronize_session="fetch"
+        )
+        db.session.commit()
+
+    def check_password(self, email, password):
+        cur_hash_password = User.query.filter_by(email=email).first().password
+        return bcrypt.checkpw(password.encode(), cur_hash_password.encode())
+
+    def change_password(self, email, password):
+        hash_password = bcrypt.hashpw(password.encode(), bcrypt.gensalt())
+        db.session.query(User).filter(User.email.ilike(email)).update(
+            {"password": hash_password.decode()}, synchronize_session="fetch"
+        )
+        db.session.commit()
+
+    def change_email(self, email, new_email):
+        db.session.query(User).filter(User.email.ilike(email)).update(
+            {"email": new_email}, synchronize_session="fetch"
+        )
+        db.session.commit()
