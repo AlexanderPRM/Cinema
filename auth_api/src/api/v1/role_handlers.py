0b15317bbf53e780ca0bafd949944b25d793d21c
Alex Shapovalov <kerrolean@yandex.ru>
2023-04-18 14:12:45 +0300

    jwt_required moved to superuser_required decorator

--- auth_api/src/api/v1/role_handlers.py
+++ auth_api/src/api/v1/role_handlers.py
@@ -3,7 +3,6 @@ from core.utils import is_uuid_valid
 from db.models import User, UserRole
 from db.postgres import db
 from flask import Blueprint, Response, abort, json, jsonify, request
-from flask_jwt_extended.view_decorators import jwt_required
 from services.exception_service import HttpExceptions
 from services.role_service import RoleService
 
@@ -15,9 +14,8 @@ def name_validate(request):
         return abort(Response(json.dumps({"error_message": "Name not specified"}), 422))
 
 
-@superuser_required
-@jwt_required(True, locations=["headers", "cookies"])
 @role_bp.route("/", methods=["GET"])
+@superuser_required
 def get_roles():
     roles = db.session.query(UserRole).all()
     roles = [{"id": role.id, "name": role.name} for role in roles]
@@ -26,9 +24,8 @@ def get_roles():
     return resp, 200
 
 
-@superuser_required
-@jwt_required(True, locations=["headers", "cookies"])
 @role_bp.route("/", methods=["POST"])
+@superuser_required
 def create_role():
     name_validate(request)
 
@@ -40,13 +37,12 @@ def create_role():
     db.session.add(role)
     db.session.commit()
 
-    resp = jsonify({"message": f"Role {name} created success"})
+    resp = jsonify({"message": f"Role {name} created successfully"})
     return resp, 201
 
 
-@superuser_required
-@jwt_required(True, locations=["headers", "cookies"])
 @role_bp.route("/<uuid:id>", methods=["DELETE"])
+@superuser_required
 def delete_role(id):
     role = db.session.query(UserRole).filter_by(id=id).first()
     if not role:
@@ -55,13 +51,12 @@ def delete_role(id):
     db.session.delete(role)
     db.session.commit()
 
-    resp = jsonify({"message": f"Role {role.id} deleted success"})
+    resp = jsonify({"message": f"Role {role.id} deleted successfully"})
     return resp, 200
 
 
-@superuser_required
-@jwt_required(True, locations=["headers", "cookies"])
 @role_bp.route("/<uuid:id>", methods=["PUT"])
+@superuser_required
 def update_role(id):
     name_validate(request)
 
@@ -72,20 +67,19 @@ def update_role(id):
     role.name = request.json["role_name"]
     db.session.commit()
 
-    resp = jsonify({"message": f"Role {role.id} updated success"})
+    resp = jsonify({"message": f"Role {role.id} updated successfully"})
     return resp, 200
 
 
-@superuser_required
-@jwt_required()
 @role_bp.route("/change_role/<uuid:id>", methods=["PUT"])
+@superuser_required
 def change_role(id):
     role_name = request.json["role_name"]
     if not is_uuid_valid(id):
         return HttpExceptions().not_valid_uuid()
     role = RoleService().get(role_name)
     if not role:
-        return HttpExceptions().not_exists("Role", id)
+        return HttpExceptions().not_exists("Role", role_name)
     user = db.session.query(User).filter_by(id=id).first()
     if not user:
         return HttpExceptions().not_exists("User", id)
@@ -94,9 +88,8 @@ def change_role(id):
     return resp, 200
 
 
-@superuser_required
-@jwt_required()
 @role_bp.route("/change_role_to_default/<uuid:id>", methods=["PUT"])
+@superuser_required
 def change_role_to_default(id):
     if not is_uuid_valid(id):
         return HttpExceptions().not_valid_uuid()
--- auth_api/src/core/permissions.py
+++ auth_api/src/core/permissions.py
@@ -1,12 +1,14 @@
 from functools import wraps
 
-from flask_jwt_extended import current_user
+from flask_jwt_extended.view_decorators import _decode_jwt_from_request, jwt_required
 
 
 def superuser_required(fn):
     @wraps(fn)
+    @jwt_required(True, locations=["headers", "cookies"])
     def wrapper(*args, **kwargs):
-        if current_user.role.name != "superuser":
+        data = _decode_jwt_from_request(locations=["headers", "cookies"], fresh=False)
+        if data[0]["role"] != "superuser":
             return {"message": "Superuser only"}, 403
         return fn(*args, **kwargs)
 
