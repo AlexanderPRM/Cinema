9dad0b6b2288e33f19f98c0fcce7b64f9affc6d2
AlexanderPRM <nathan920@yandex.ru>
2023-04-21 09:35:50 +0300

    fix:linters

--- auth_api/tests/functional/src/test_user.py
+++ auth_api/tests/functional/src/test_user.py
@@ -276,6 +276,7 @@ async def test_login_history(
     response_data = json.loads(response_text)
     assert len(response_data["login_history"]) == 1
 
+
 @pytest.mark.parametrize(
     "expected_answer",
     [
