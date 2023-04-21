68a5f264097676697f404e7783c8ee91ffc52103
dimitrieidifesk <123076304+dimitrieidifesk@users.noreply.github.com>
2023-04-21 18:10:20 +0300

    add flasgger

--- auth_api/src/app.py
+++ auth_api/src/app.py
@@ -1,15 +1,15 @@
 import logging
 
 import uvicorn
-from flask import Flask
-from flask_migrate import Migrate
-
 from api import api_blueprint_v1
 from api.v1.user_handlers import jwt
 from core.config import config
 from core.logger import LOGGING
 from db.postgres import db
 from db.redis import redis_db
+from flasgger import Swagger
+from flask import Flask
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 migrate = Migrate(app, db)
@@ -18,6 +18,8 @@ app.register_blueprint(api_blueprint_v1)
 
 with app.app_context():
     from cli.superuser import create_super_user  # noqa: 402
+
+swagger = Swagger(app, template_file="openapi.yaml")
 
 
 def init_redis(app: Flask):
