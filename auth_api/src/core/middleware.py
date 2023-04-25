41972bcdd7cce56f0d62df51a1f2bc89c62072e9
Alex Shapovalov <kerrolean@yandex.ru>
2023-04-25 22:26:46 +0300

    fix isort/black

--- auth_api/src/core/middleware.py
+++ auth_api/src/core/middleware.py
@@ -12,12 +12,13 @@ MAX_TOKENS = 100000
 SEC_FOR_TOKEN = 0.001
 
 redis_conn = redis.Redis(host=config.AUTH_REDIS_HOST, port=config.AUTH_REDIS_PORT, db=1)
-redis_conn.set('tokens', MAX_TOKENS)
-redis_conn.set('last_refill_time', time.time())
+redis_conn.set("tokens", MAX_TOKENS)
+redis_conn.set("last_refill_time", time.time())
+
 
 def check_rate_limit():
-    tokens = int(redis_conn.get('tokens'))
-    last_refill_time = float(redis_conn.get('last_refill_time'))
+    tokens = int(redis_conn.get("tokens"))
+    last_refill_time = float(redis_conn.get("last_refill_time"))
 
     elapsed_time = time.time() - float(last_refill_time)
     tokens_to_add = int(elapsed_time / SEC_FOR_TOKEN)
@@ -26,10 +27,10 @@ def check_rate_limit():
     tokens = min(tokens + tokens_to_add, MAX_TOKENS)
 
     if tokens_to_add > 0:
-        redis_conn.set('last_refill_time', time.time() - float(remainder))
+        redis_conn.set("last_refill_time", time.time() - float(remainder))
 
     if tokens > 0:
-        redis_conn.set('tokens', tokens - 1)
+        redis_conn.set("tokens", tokens - 1)
     else:
         abort(
             Response(
