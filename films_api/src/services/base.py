2bd21801d6fdf6a3f4dc6fbef430d7cb5628bca0
dimitrieidifesk <123076304+dimitrieidifesk@users.noreply.github.com>
2023-05-01 21:33:57 +0300

    add verify_jwt function

--- films_api/src/services/base.py
+++ films_api/src/services/base.py
@@ -1,6 +1,11 @@
 from abc import abstractmethod
 from typing import Any
 
+from core.config import config
+from fastapi import HTTPException
+from jose import JWTError, jwt
+from models.film import User
+
 
 class BaseService:
     @abstractmethod
@@ -10,3 +15,15 @@ class BaseService:
     @abstractmethod
     async def get_by_param(self, *args, **kwargs) -> Any:
         pass
+
+
+async def verify_jwt(request):
+    token_cookie = request.cookies.get("access_token_cookie")
+
+    if not token_cookie:
+        raise HTTPException(status_code=401, detail="Token not found in cookies")
+    try:
+        payload = jwt.decode(token_cookie, config.JWT_SECRET, algorithms=["HS256"])
+        return User(username=payload.get("sub"))
+    except Exception:
+        raise HTTPException(status_code=401, detail="Invalid or expired token")
