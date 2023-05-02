bf5c5321da02e98da71a909ac5c44c0fac03689c
Alex Shapovalov <kerrolean@yandex.ru>
2023-05-02 15:44:58 +0300

    graceful degradation for rate_limit

--- auth_api/src/core/middleware.py
+++ auth_api/src/core/middleware.py
@@ -11,30 +11,39 @@ from core.config import config
 MAX_TOKENS = config.MAX_TOKENS
 SEC_FOR_TOKEN = config.SEC_FOR_TOKEN
 
-redis_conn = redis.Redis(host=config.AUTH_REDIS_HOST, port=config.AUTH_REDIS_PORT, db=1)
-redis_conn.set("tokens_amount", MAX_TOKENS)
-redis_conn.set("last_refill_time", time.time())
 
+try:
+    redis_conn = redis.Redis(host=config.AUTH_REDIS_HOST, port=config.AUTH_REDIS_PORT, db=1)
+    redis_conn.set("tokens_amount", MAX_TOKENS)
+    redis_conn.set("last_refill_time", time.time())
+except redis.RedisError as e:
+    # можно записать в логи
+    redis_conn = None
 
 def check_rate_limit():
-    tokens_amount = int(redis_conn.get("tokens_amount"))
-    last_refill_time = float(redis_conn.get("last_refill_time"))
+    if redis_conn is None:
+        # если Redis недоступен, отключаем rate limiting
+        # можно записать в логи
+        return
 
-    time_diff = time.time() - float(last_refill_time)
-    tokens_to_add = int(time_diff / SEC_FOR_TOKEN)
-    remainder = Decimal(f"{time_diff}") % Decimal(f"{SEC_FOR_TOKEN}")
+    try:
+        tokens_amount = int(redis_conn.get("tokens_amount"))
+        last_refill_time = float(redis_conn.get("last_refill_time"))
 
-    tokens_amount = min(tokens_amount + tokens_to_add, MAX_TOKENS)
+        time_diff = time.time() - float(last_refill_time)
+        tokens_to_add = int(time_diff / SEC_FOR_TOKEN)
+        remainder = Decimal(f"{time_diff}") % Decimal(f"{SEC_FOR_TOKEN}")
 
-    if tokens_to_add > 0:
-        redis_conn.set("last_refill_time", time.time() - float(remainder))
+        tokens_amount = min(tokens_amount + tokens_to_add, MAX_TOKENS)
 
-    if tokens_amount > 0:
+        if tokens_to_add > 0:
+            redis_conn.set("last_refill_time", time.time() - float(remainder))
+
+        if tokens_amount < 1:
+            abort(Response(json.dumps({"error": "Too Many Requests"}), status=HTTPStatus.TOO_MANY_REQUESTS))
         redis_conn.set("tokens_amount", tokens_amount - 1)
-    else:
-        abort(
-            Response(
-                json.dumps({"message": "Too many requests, try later."}),
-                HTTPStatus.TOO_MANY_REQUESTS,
-            )
-        )
+
+    except redis.RedisError as e:
+        # в случае ошибки Redis, отключаем rate limiting
+        # можно записать в логи
+        return
