2b150c541cdfb21fb3297bdd6759f33e40f293de
dimitrieidifesk <123076304+dimitrieidifesk@users.noreply.github.com>
2023-05-04 21:01:57 +0300

    Added jwt check to auth's redis

--- films_api/src/services/base.py
+++ films_api/src/services/base.py
@@ -4,6 +4,7 @@ from typing import Any
 from core.config import config
 from fastapi import HTTPException
 from jose import jwt
+from redis.asyncio import Redis
 
 
 class BaseService:
@@ -18,11 +19,15 @@ class BaseService:
 
 async def verify_jwt(request):
     token_cookie = request.cookies.get("access_token_cookie")
+    redis_auth = Redis(host=config.AUTH_REDIS_HOST, port=config.AUTH_REDIS_PORT)
 
     if not token_cookie:
         raise HTTPException(status_code=401, detail="Token not found in cookies")
     try:
         payload = jwt.decode(token_cookie, config.JWT_SECRET, algorithms=["HS256"])
+        jti = payload.get("jti")
+        if await redis_auth.get(jti + "_access"):
+            raise HTTPException(status_code=401, detail="Token can no longer be used")
         return payload.get("sub")
     except Exception:
         raise HTTPException(status_code=401, detail="Invalid or expired token")
