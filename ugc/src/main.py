ad450a009b475094c04c47492aca9e163dc51063
AlexanderPRM <nathan920@yandex.ru>
2023-05-17 13:39:51 +0300

    small change infrast.

---
+++ ugc/src/main.py
@@ -0,0 +1,19 @@
+import logging
+
+import uvicorn
+from core.config import config
+from fastapi import FastAPI
+from fastapi.responses import ORJSONResponse
+
+app = FastAPI(
+    title=config.UGC_PROJECT_NAME,
+    description="Отслеживание прогресса просмотра фильма",
+    version=config.UGC_PROJECT_VERSION,
+    docs_url="/api/openapi",
+    openapi_url="/api/openapi.json",
+    default_response_class=ORJSONResponse,
+)
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", port=8001, log_level=logging.DEBUG)
