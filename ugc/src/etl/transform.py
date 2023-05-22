f599a51ec66b7ccbfc3c7a358be454e54aa1017e
dimitrieidifesk <123076304+dimitrieidifesk@users.noreply.github.com>
2023-05-22 21:49:38 +0300

    Add files via upload

--- ugc/src/etl/transform.py
+++ ugc/src/etl/transform.py
@@ -7,12 +7,21 @@ class Transform:
         for entry in data:
             key = entry[0]
             value = entry[1]
+            ids = key.split(";")
+            movie_id = ids[1].split("=")[1]
+            user_id = ids[0].split("=")[1]
+            times = value.split("_")
+            updated_at = times[1]
+            timestamp=times[0]
             data_to_save.append(
                 Entry(
-                    movie_id=key.split(";")[1].split("=")[1],
-                    user_id=key.split(";")[0].split("=")[1],
-                    updated_at=value.split("_")[1],
-                    timestamp=value.split("_")[0],
+                    movie_id=movie_id,
+                    user_id=user_id,
+                    updated_at=updated_at,
+                    timestamp=timestamp,
                 )
             )
         return data_to_save
+
+
+
