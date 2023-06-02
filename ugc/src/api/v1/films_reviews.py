a1aaa3a73f11f8a2ce72ee47817c4c9e34a3ba17
Alex Shapovalov <kerrolean@yandex.ru>
2023-06-02 13:02:18 +0300

    fix isort

--- ugc/src/api/v1/films_reviews.py
+++ ugc/src/api/v1/films_reviews.py
@@ -6,10 +6,10 @@ from uuid import UUID
 from bson.objectid import ObjectId
 from core.config import collections_names
 from core.jwt import JWTBearer
-from models.ugc_models import FilmReview
 from db.mongo import Mongo, get_db
 from fastapi import APIRouter, Body, Depends, Path
 from fastapi.exceptions import HTTPException
+from models.ugc_models import FilmReview
 
 router = APIRouter()
 
