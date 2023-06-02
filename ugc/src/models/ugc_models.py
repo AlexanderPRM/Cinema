c5a4794cfe2caf8ed06e408b3d678b2a77220b12
Alex Shapovalov <kerrolean@yandex.ru>
2023-06-02 12:59:30 +0300

    models unite

---
+++ ugc/src/models/ugc_models.py
@@ -0,0 +1,13 @@
+from datetime import datetime
+from typing import Optional
+
+from base import OrjsonBaseModel
+
+class Bookmark(OrjsonBaseModel):
+    user_id: str
+    film_id: str
+    created_at: Optional[datetime] = None
+
+
+class FilmReview(OrjsonBaseModel):
+    text: str
