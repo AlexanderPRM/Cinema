9912bccb6103ae086fe1cf4853328f2476a01e65
dimitrieidifesk <123076304+dimitrieidifesk@users.noreply.github.com>
2023-06-06 19:26:20 +0300

    fix linters

--- ugc/src/core/logging_setup.py
+++ ugc/src/core/logging_setup.py
@@ -5,4 +5,8 @@ from core.config import project_settings
 
 LOGGER = logging.getLogger(__name__)
 LOGGER.setLevel(logging.INFO)
-LOGGER.addHandler(logstash.LogstashHandler(project_settings.LOGSTASH_HOST, project_settings.LOGSTASH_PORT, version=1))
+LOGGER.addHandler(
+    logstash.LogstashHandler(
+        project_settings.LOGSTASH_HOST, project_settings.LOGSTASH_PORT, version=1
+    )
+)
