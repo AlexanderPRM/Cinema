0ec1982f67cea6cef7ae575f0f66a5ce6f5c9d8d
dimitrieidifesk <123076304+dimitrieidifesk@users.noreply.github.com>
2023-06-14 12:34:09 +0300

    fix linters

--- notifications/components/adminpanel/adminpanel/urls.py
+++ notifications/components/adminpanel/adminpanel/urls.py
@@ -13,9 +13,17 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from app import views
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.urls import path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("", views.index, name="home"),
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += staticfiles_urlpatterns()
