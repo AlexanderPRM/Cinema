06c77a8ce3c7c8e152e641471aec98af419a7e0b
AlexanderPRM <nathan920@yandex.ru>
2023-06-18 16:00:20 +0300

    fix:worker send

--- notifications/components/worker/core/config.py
+++ notifications/components/worker/core/config.py
@@ -4,6 +4,8 @@ from pydantic import BaseSettings, Field
 class WorkerSettings(BaseSettings):
     JWT_SECRET: str
     JWT_ADMIN_ROLE: str
+    NOTF_ELASTICEMAIL_API_KEY: str
+    NOTF_ELASTICEMAIL_FROM_EMAIL: str
     AUTH_URL: str = Field("http://nginx/auth/api/v1/user/")
 
     class Config:
--- notifications/components/worker/main.py
+++ notifications/components/worker/main.py
@@ -1,8 +1,7 @@
 import asyncio
-import os
 
 import asyncpg
-from core.config import postgres_settings
+from core.config import postgres_settings, worker_setting
 from db.postgres import PostgreSQLConsumer
 from db.rabbitmq import RabbitConsumer
 from worker import Worker
@@ -10,10 +9,10 @@ from worker import Worker
 
 async def main():
     client = Worker(
-        os.getenv("NOTF_ELASTICEMAIL_API_KEY"),
-        os.getenv("NOTF_ELASTICEMAIL_FROM_EMAIL"),
-        RabbitConsumer(),
-        PostgreSQLConsumer(
+        api_key=worker_setting.NOTF_ELASTICEMAIL_API_KEY,
+        from_email=worker_setting.NOTF_ELASTICEMAIL_FROM_EMAIL,
+        rabbitmq_client=RabbitConsumer(),
+        postgres_client=PostgreSQLConsumer(
             await asyncpg.connect(
                 user=postgres_settings.NOTF_POSTGRES_USER,
                 password=postgres_settings.NOTF_POSTGRES_PASSWORD,
