4d93e98151c18111f5814e72fb2c6f17853ccd5e
AlexanderPRM <nathan920@yandex.ru>
2023-06-24 13:25:00 +0300

    refactor:events to lifespan

--- notifications/components/notf_api/app.py
+++ notifications/components/notf_api/app.py
@@ -1,7 +1,20 @@
+from contextlib import asynccontextmanager
+
 from core.config import project_settings
 from db.rabbitmq import RabbitWorker
 from fastapi import FastAPI
 from fastapi.responses import ORJSONResponse
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    rabbit_worker = RabbitWorker()
+    await rabbit_worker.get_connection()
+    await rabbit_worker.make_queues()
+    app.state.rabbit_worker = rabbit_worker
+    yield
+    await app.state.rabbit_worker.close_connection()
+
 
 app = FastAPI(
     title=project_settings.NOTF_PROJECT_NAME,
@@ -10,17 +23,5 @@ app = FastAPI(
     docs_url="/api/openapi",
     openapi_url="/api/openapi.json",
     default_response_class=ORJSONResponse,
+    lifespan=lifespan,
 )
-
-
-@app.on_event("startup")
-async def startup():
-    rabbit_worker = RabbitWorker()
-    await rabbit_worker.get_connection()
-    await rabbit_worker.make_queues()
-    app.state.rabbit_worker = rabbit_worker
-
-
-@app.on_event("shutdown")
-async def shutdown():
-    await app.state.rabbit_worker.close_connection()
