a4591f4e90922fed9f67bb310bae08ffd413950d
AlexanderPRM <nathan920@yandex.ru>
2023-07-09 16:38:52 +0300

    components:docker and requirements

---
+++ billing/components/billing_api/run_billing.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+echo "Waiting for PSQL..."
+
+while ! nc -z $POSTGRES_HOST $POSTGRES_PORT; do
+    sleep 0.1
+done
+
+echo "PSQL started"
+
+cd src
+gunicorn main:app --workers 4 --worker-class uvicorn.workers.UvicornWorker --bind 0.0.0.0:8000
