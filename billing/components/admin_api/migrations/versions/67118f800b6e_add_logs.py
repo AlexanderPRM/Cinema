21d1ab8205c9c49bba96c6a181cfb05de5758113
Dimitriev Denis <Deniska-1616@mail.ru>
2023-07-13 19:24:08 +0300

    fix linters

---
+++ billing/components/admin_api/migrations/versions/67118f800b6e_add_logs.py
@@ -0,0 +1,23 @@
+"""add_logs
+
+Revision ID: 67118f800b6e
+Revises: a9fb2215112b
+Create Date: 2023-07-13 10:03:03.305384
+
+"""
+import sqlalchemy as sa
+from alembic import op
+
+# revision identifiers, used by Alembic.
+revision = '67118f800b6e'
+down_revision = 'a9fb2215112b'
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
---
+++ billing/components/admin_api/src/core/models.py
@@ -0,0 +1,53 @@
+import datetime
+import uuid
+
+from sqlalchemy import Boolean, Column, DateTime, Enum, Integer, String
+from sqlalchemy.dialects.postgresql import ENUM, UUID
+from sqlalchemy.orm import declarative_base
+
+Base = declarative_base()
+
+
+class Subscriptions(Base):
+    __tablename__ = "subscriptions_tiers"
+    title = Column(String, nullable=False)
+    subscribe_id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    duratation = Column(DateTime, nullable=False)
+    cost = Column(Integer, nullable=False)
+    description = Column(String, nullable=False)
+    discount = Column(Integer, default=0, nullable=False)
+    discount_duratation = Column(DateTime, nullable=True)
+    created_at = Column(DateTime(timezone=True), default=datetime.datetime.now)
+    updated_at = Column(
+        DateTime(timezone=True), default=datetime.datetime.now, onupdate=datetime.datetime.now
+    )
+
+
+class SubscriptionsUsers(Base):
+    __tablename__ = "subscriptions"
+    user_id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    transaction_id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    subscribe_id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    ttl = Column(DateTime, nullable=False)
+    auto_renewal = Column(Boolean, nullable=False)
+    created_at = Column(DateTime(timezone=True), default=datetime.datetime.now)
+    updated_at = Column(
+        DateTime(timezone=True), default=datetime.datetime.now, onupdate=datetime.datetime.now
+    )
+
+
+class TransactionsLog(Base):
+    __tablename__ = "transactions_log"
+    user_id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    transaction_id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    value = Column(Integer, nullable=False)
+    provider = Column(String, nullable=False)
+    idempotency_key_ttl = Column(DateTime, nullable=False)
+    idempotency_key = Column(UUID(as_uuid=True), nullable=False)
+    StatusEnum = ENUM("SUCCESS", "ERROR", "WAITING", name="status_enum")
+    operate_status = Column(StatusEnum, default="WAITING", nullable=False)
+    payment_details = Column(String, nullable=True)
+    created_at = Column(DateTime(timezone=True), default=datetime.datetime.now)
+    updated_at = Column(
+        DateTime(timezone=True), default=datetime.datetime.now, onupdate=datetime.datetime.now
+    )
