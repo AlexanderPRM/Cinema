9af54e4794c243f4585c30a7bed409d7c32b2a68
AlexanderPRM <nathan920@yandex.ru>
2023-07-16 13:52:04 +0300

    refactor:migrations

--- billing/components/admin_api/migrations/env.py
+++ billing/components/admin_api/migrations/env.py
@@ -4,8 +4,7 @@ from logging.config import fileConfig
 from alembic import context
 from dotenv import load_dotenv
 from sqlalchemy import engine_from_config, pool
-
-from billing.components.admin_api.src.db.models import Base
+from src.db.models import Base
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
--- billing/components/admin_api/src/db/models.py
+++ billing/components/admin_api/src/db/models.py
@@ -1,8 +1,8 @@
 import datetime
 import uuid
 
-from sqlalchemy import Boolean, Column, DateTime, Integer, String
-from sqlalchemy.dialects.postgresql import ENUM, UUID
+from sqlalchemy import Column, DateTime, Integer, String
+from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.orm import declarative_base
 
 Base = declarative_base()
@@ -17,36 +17,6 @@ class Subscriptions(Base):
     description = Column(String, nullable=False)
     discount = Column(Integer, default=0, nullable=False)
     discount_duratation = Column(DateTime, nullable=True)
-    created_at = Column(DateTime(timezone=True), default=datetime.datetime.now)
-    updated_at = Column(
-        DateTime(timezone=True), default=datetime.datetime.now, onupdate=datetime.datetime.now
-    )
-
-
-class SubscriptionsUsers(Base):
-    __tablename__ = "subscriptions"
-    user_id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
-    transaction_id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
-    subscribe_id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
-    ttl = Column(DateTime, nullable=False)
-    auto_renewal = Column(Boolean, nullable=False)
-    created_at = Column(DateTime(timezone=True), default=datetime.datetime.now)
-    updated_at = Column(
-        DateTime(timezone=True), default=datetime.datetime.now, onupdate=datetime.datetime.now
-    )
-
-
-class TransactionsLog(Base):
-    __tablename__ = "transactions_log"
-    user_id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
-    transaction_id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
-    value = Column(Integer, nullable=False)
-    provider = Column(String, nullable=False)
-    idempotency_key_ttl = Column(DateTime, nullable=False)
-    idempotency_key = Column(UUID(as_uuid=True), nullable=False)
-    StatusEnum = ENUM("SUCCESS", "ERROR", "WAITING", name="status_enum")
-    operate_status = Column(StatusEnum, default="WAITING", nullable=False)
-    payment_details = Column(String, nullable=True)
     created_at = Column(DateTime(timezone=True), default=datetime.datetime.now)
     updated_at = Column(
         DateTime(timezone=True), default=datetime.datetime.now, onupdate=datetime.datetime.now
