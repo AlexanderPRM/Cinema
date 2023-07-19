3a22b84318d29202b5e6c55d7507d6d467f577fe
Dimitriev Denis <Deniska-1616@mail.ru>
2023-07-19 12:31:24 +0300

    add: rabbitmq settings

--- billing/components/core/config.py
+++ billing/components/core/config.py
@@ -2,11 +2,17 @@ from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
+    SUBSCRIPTIONS_TABLE: str
     JWT_SECRET: str
     JWT_ADMIN_ROLE: str
-    YOOKASSA_SHOP_ID: int
-    YOOKASSA_SHOP_SECRET: str
-    PAYMENT_REDIRECT_URL: str
+    POSTGRESQL_URL: str
+    POSTGRESQL_URL_ASYNC: str
+    SUBSCRIPTIONS_USERS_TABLE: str
+    TRANSACTIONS_LOG_TABLE: str
+    REDIS_HOST: str
+    REDIS_PORT: int
+    YOOKASSA_SHOP_ID: str
+    YOOKASSA_SECRET_KEY: str
 
     class Config:
         case_sensitive = True
@@ -19,15 +25,26 @@ class PostgreSQLSettings(BaseSettings):
     POSTGRES_HOST: str
     POSTGRES_PORT: str
     POSTGRES_DB: str
-    POSTGRESQL_URL: str
-    SUBSCRIPTIONS_TABLE: str
     SUBSCRIPTIONS_USERS_TABLE: str
-    TRANSACTIONS_LOG_TABLE: str
 
     class Config:
         case_sensitive = True
         env_file = "config.env"
 
 
+class RabbitMQSettings(BaseSettings):
+    BILLING_QUEUE_NOTIFICATIONS: str
+    BILLING_QUEUE_AUTH: str
+    RABBITMQ_USER: str
+    RABBITMQ_PASS: str
+    RABBITMQ_HOST: str
+    RABBITMQ_PORT: int
+
+    class Config:
+        case_sensitive = True
+        env_file = "config.env"
+
+
+rabbit_settings = RabbitMQSettings()
 config = Settings()
 postgres_settings = PostgreSQLSettings()
