3a198ff5b8f7891aeaf55fa458e4cfefd9d2f79e
AlexanderPRM <nathan920@yandex.ru>
2023-07-20 21:27:40 +0300

    fix:linters

--- billing/components/payment_api/src/services/providers_workers/yookassa.py
+++ billing/components/payment_api/src/services/providers_workers/yookassa.py
@@ -10,10 +10,10 @@ class YooKassaProviderWorker(BaseProviderWorker):
         notification_object = WebhookNotification(event_json)
         payment = notification_object.object
         if payment.status == "canceled":
-            psql.update_transaction_status_to_error(payment['id'])
+            psql.update_transaction_status_to_error(payment["id"])
             return
         # if payment.status == "succeeded":
-        psql.update_transaction_status_to_success(payment['id'])
+        psql.update_transaction_status_to_success(payment["id"])
         psql.create_subscription(payment)
         return
 
