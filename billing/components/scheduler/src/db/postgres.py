675a5b4ff91c3844a0b9dacf3c6b75057f55dc08
Dimitriev Denis <Deniska-1616@mail.ru>
2023-07-20 11:49:14 +0300

    fix flake8

--- billing/components/scheduler/src/db/postgres.py
+++ billing/components/scheduler/src/db/postgres.py
@@ -21,11 +21,11 @@ class PostgreSQLProducer:
     )
     async def get_ended_subs(self, previous_run_time):
         query = (
-            f"SELECT user_id, transaction_id, subscribe_id, ttl, auto_renewal, created_at, "
-            f"updated_at FROM %s "
-            f"WHERE ttl <= '%s' "
-            f"AND ttl >= '%s' "
-            f"GROUP BY transaction_id, user_id, subscribe_id ORDER BY ttl;"
+            "SELECT user_id, transaction_id, subscribe_id, ttl, auto_renewal, created_at, "
+            "updated_at FROM %s "
+            "WHERE ttl <= '%s' "
+            "AND ttl >= '%s' "
+            "GROUP BY transaction_id, user_id, subscribe_id ORDER BY ttl;"
             % (
                 postgres_settings.SUBSCRIPTIONS_USERS_TABLE,
                 datetime.datetime.now(),
@@ -36,19 +36,19 @@ class PostgreSQLProducer:
 
     async def get_subscriprion_cost(self, sub_id):
         query = (
-            f"SELECT cost "
-            f"FROM {postgres_settings.SUBSCRIPTIONS_TABLE} "
-            f"WHERE subscribe_id = '{sub_id}' "
-            f"FROM %s "
-            f"WHERE subscribe_id = '%s' " % (postgres_settings.SUBSCRIPTIONS_TABLE, sub_id)
+            "SELECT cost "
+            "FROM {postgres_settings.SUBSCRIPTIONS_TABLE} "
+            "WHERE subscribe_id = '{sub_id}' "
+            "FROM %s "
+            "WHERE subscribe_id = '%s' " % (postgres_settings.SUBSCRIPTIONS_TABLE, sub_id)
         )
         return await self.connection.fetch(query)
 
     async def get_transaction_currency(self, transaction_id):
         query = (
-            f"SELECT currency "
-            f"FROM %s "
-            f"WHERE transaction_id = '%s' ORDER BY updated_at LIMIT 1"
+            "SELECT currency "
+            "FROM %s "
+            "WHERE transaction_id = '%s' ORDER BY updated_at LIMIT 1"
             % (postgres_settings.TRANSACTIONS_LOG_TABLE, transaction_id)
         )
         return await self.connection.fetch(query)
