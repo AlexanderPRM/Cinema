4513cd4e2f2123452ad5e910d8eda1cc7f8f2bb0
AlexanderPRM <nathan920@yandex.ru>
2023-07-21 18:24:01 +0300

    add:architecture link

--- notifications/components/adminpanel/adminpanel/settings.py
+++ notifications/components/adminpanel/adminpanel/settings.py
@@ -122,7 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_ROOT = "opt/adminpanel/static"
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
